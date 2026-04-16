5dd347030b309c3e09808ac14a3696a34dbc2037
Juan Esteban Katz <jestebankatz@gmail.com>
2026-04-15 20:53:23 -0500

    primeros pasos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { CatsComponent } from './cats/cats.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dogs', component: DogsComponent },
+  { path: 'cats', component: CatsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: '**', redirectTo: '' }
+];
